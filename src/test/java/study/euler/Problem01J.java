cb606075b1b6fd8aeeb12dbba86f83d4724d0f6f
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-14 20:09:20 -0700

    added

---
+++ src/test/java/study/euler/Problem01J.java
@@ -0,0 +1,28 @@
+package study.euler;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Problem #1: Add all the natural numbers below one thousand that
+ * are multiples of 3 or 5.
+ * <p/>
+ * Answer: 233168
+ */
+public class Problem01J {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void solve() {
+        int solution = 0;
+        for (int n = 1; n < 1000; n++) {
+            if ((n % 3 == 0) || (n % 5 == 0)) solution += n;
+        }
+
+        logger.info(String.format("solution = %d", solution));
+
+        Assert.assertTrue(solution == 233168);
+    }
+}
